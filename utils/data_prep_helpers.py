9686f4dd7f259b98e4d5dc59d646693c511fbcc2
john culnan <jmculnan@email.arizona.edu>
2022-01-18 14:19:30 -0700

    added sampler to this repo

--- utils/data_prep_helpers.py
+++ utils/data_prep_helpers.py
@@ -116,6 +116,61 @@ class MultitaskTestObject(object):
         """
         self.loss_multiplier = multiplier
 
+# todo: test out using this sampler
+class BatchSchedulerSampler(torch.utils.data.sampler.Sampler):
+    """
+    iterate over tasks and provide a random batch per task in each mini-batch
+    Slightly altered from: https://gist.github.com/bomri/d93da3e6f840bb93406f40a6590b9c48
+    """
+    def __init__(self, dataset, batch_size):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.number_of_datasets = len(dataset.datasets)
+        self.largest_dataset_size = max([len(cur_dataset.samples) for cur_dataset in dataset.datasets])
+
+    def __len__(self):
+        return self.batch_size * math.ceil(self.largest_dataset_size / self.batch_size) * len(self.dataset.datasets)
+
+    def __iter__(self):
+        samplers_list = []
+        sampler_iterators = []
+        for dataset_idx in range(self.number_of_datasets):
+            cur_dataset = self.dataset.datasets[dataset_idx]
+            sampler = RandomSampler(cur_dataset)
+            samplers_list.append(sampler)
+            cur_sampler_iterator = sampler.__iter__()
+            sampler_iterators.append(cur_sampler_iterator)
+
+        push_index_val = [0] + self.dataset.cumulative_sizes[:-1]
+        step = self.batch_size * self.number_of_datasets
+        samples_to_grab = self.batch_size
+        # for this case we want to get all samples in dataset, this force us to resample from the smaller datasets
+        epoch_samples = self.largest_dataset_size * self.number_of_datasets
+
+        final_samples_list = []  # this is a list of indexes from the combined dataset
+        for _ in range(0, epoch_samples, step):
+            for i in range(self.number_of_datasets):
+                cur_batch_sampler = sampler_iterators[i]
+                cur_samples = []
+                for _ in range(samples_to_grab):
+                    try:
+                        cur_sample_org = cur_batch_sampler.__next__()
+                        cur_sample = cur_sample_org + push_index_val[i]
+                        cur_samples.append(cur_sample)
+                    except StopIteration:
+                        # stop trying to add samples and continue on in the next dataset
+                        break
+                        # got to the end of iterator - restart the iterator and continue to get samples
+                        # until reaching "epoch_samples"
+                        # sampler_iterators[i] = samplers_list[i].__iter__()
+                        # cur_batch_sampler = sampler_iterators[i]
+                        # cur_sample_org = cur_batch_sampler.__next__()
+                        # cur_sample = cur_sample_org + push_index_val[i]
+                        # cur_samples.append(cur_sample)
+                final_samples_list.extend(cur_samples)
+
+        return iter(final_samples_list)
+
 
 class Glove(object):
     def __init__(self, glove_dict):
@@ -329,10 +384,6 @@ def get_data_samples(data_list, num_samples):
     return new_samples
 
 
-# I found this method recently, in a discussion that sometimes weights are better
-# served in the loss function than in a sampler.  What you were returning below
-# seem to be counts, not weights.  These are automatically calculated by sklearn, and
-# apparently based off imbalanced logistic regression.  Let's see if they help!
 def get_class_weights(data_tensors_dict, data_type):
     if data_type == "meld":
         y_tensor = data_tensors_dict["all_emotions"]
